2e9c044b7d1dae02c5216f1429cf5c8bc1e5d9e6
chondosha1234 <jmiller61193@gmail.com>
2023-04-05 17:40:22 -0400

    added detailfragment class and layout

--- app/src/main/java/com/chondosha/criminalintent/CrimeDetailFragment.kt
+++ app/src/main/java/com/chondosha/criminalintent/CrimeDetailFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.widget.doOnTextChanged
 import androidx.fragment.app.Fragment
 import com.chondosha.criminalintent.databinding.FragmentCrimeDetailBinding
 import java.util.*
@@ -33,5 +34,24 @@ class CrimeDetailFragment: Fragment() {
         binding = FragmentCrimeDetailBinding.inflate(layoutInflater, container, false)
         return binding.root
 
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.apply {
+            crimeTitle.doOnTextChanged { text, _, _, _ ->
+                crime = crime.copy(title = text.toString())
+            }
+
+            crimeDate.apply {
+                text = crime.date.toString()
+                isEnabled = false
+            }
+
+            crimeSolved.setOnCheckedChangeListener {_, isChecked ->
+                crime = crime.copy(isSolved = isChecked)
+            }
+        }
     }
 }
