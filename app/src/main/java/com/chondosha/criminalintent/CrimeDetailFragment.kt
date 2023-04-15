eeaf20ca18673b04a3b6864d7702da2f957e4ff6
chondosha1234 <jmiller61193@gmail.com>
2023-04-15 16:12:30 -0400

    added files.xml and connected it to fileprovider, added implicit intent to launch camera, and added picture util to display correctly sized image

--- app/src/main/java/com/chondosha/criminalintent/CrimeDetailFragment.kt
+++ app/src/main/java/com/chondosha/criminalintent/CrimeDetailFragment.kt
@@ -9,6 +9,8 @@ import android.provider.ContactsContract
 import android.text.format.DateFormat
 import android.view.*
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.FileProvider
+import androidx.core.view.doOnLayout
 import androidx.core.widget.doOnTextChanged
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.setFragmentResultListener
@@ -20,6 +22,7 @@ import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.chondosha.criminalintent.databinding.FragmentCrimeDetailBinding
 import kotlinx.coroutines.launch
+import java.io.File
 import java.util.*
 
 private const val DATE_FORMAT = "EEE, MMM, dd"
@@ -33,11 +36,14 @@ class CrimeDetailFragment: Fragment() {
             "Cannot access binding because it is null. Is the view visible?"
         }
 
+
     private val args: CrimeDetailFragmentArgs by navArgs()
+
 
     private val crimeDetailViewModel: CrimeDetailViewModel by viewModels {
         CrimeDetailViewModelFactory(args.crimeId)
     }
+
 
     private val selectSuspect = registerForActivityResult(
         ActivityResultContracts.PickContact()
@@ -45,10 +51,25 @@ class CrimeDetailFragment: Fragment() {
         uri?.let { parseContactSelection(it) }
     }
 
+
+    private val takePhoto = registerForActivityResult(
+        ActivityResultContracts.TakePicture()
+    ) { didTakePhoto ->
+        if (didTakePhoto && photoName != null) {
+            crimeDetailViewModel.updateCrime { oldCrime ->
+                oldCrime.copy(photoFileName = photoName)
+            }
+        }
+    }
+
+    private var photoName: String? = null
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setHasOptionsMenu(true)
     }
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -59,6 +80,7 @@ class CrimeDetailFragment: Fragment() {
         return binding.root
 
     }
+
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -80,8 +102,31 @@ class CrimeDetailFragment: Fragment() {
                 selectSuspect.launch(null)
             }
 
+            // check to make sure device has app to use, if not gray out button
             val selectSuspectIntent = selectSuspect.contract.createIntent(requireContext(), null)
             crimeSuspect.isEnabled = canResolveIntent(selectSuspectIntent)
+
+            crimeCamera.setOnClickListener {
+                photoName = "IMG_${Date()}.JPG"   // filename has timestamp for uniqueness
+                val photoFile = File(requireContext().applicationContext.filesDir, photoName)
+                val photoUri = FileProvider.getUriForFile(
+                    requireContext(),
+                    "com.chondosha.criminalintent.fileprovider",
+                    photoFile
+                )
+                takePhoto.launch(photoUri)
+            }
+
+            // check to make sure device has app to use, if not gray out button
+            // dummyuri required because TakePhoto requires non-null input in intent
+            val dummyFile = File(requireContext().applicationContext.filesDir, "dummy.jpg")
+            val dummyUri = FileProvider.getUriForFile(
+                requireContext(),
+                "com.chondosha.criminalintent.fileprovider",
+                dummyFile
+            )
+            val captureImageIntent = takePhoto.contract.createIntent(requireContext(), dummyUri)
+            crimeCamera.isEnabled = canResolveIntent(captureImageIntent)
         }
 
         viewLifecycleOwner.lifecycleScope.launch {
@@ -98,15 +143,18 @@ class CrimeDetailFragment: Fragment() {
         }
     }
 
+
      override fun onDestroyView() {
          super.onDestroyView()
          _binding = null
      }
 
+
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
         super.onCreateOptionsMenu(menu, inflater)
         inflater.inflate(R.menu.fragment_crime_detail, menu)
     }
+
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         return when (item.itemId) {
@@ -125,6 +173,7 @@ class CrimeDetailFragment: Fragment() {
             else -> super.onOptionsItemSelected(item)
         }
     }
+
 
     private fun updateUi(crime: Crime) {
         binding.apply {
@@ -155,8 +204,11 @@ class CrimeDetailFragment: Fragment() {
             crimeSuspect.text = crime.suspect.ifEmpty {
                 getString(R.string.crime_suspect_text)
             }
+
+            updatePhoto(crime.photoFileName)
         }
     }
+
 
     private fun getCrimeReport(crime: Crime): String {
         val solvedString = if (crime.isSolved) {
@@ -197,5 +249,30 @@ class CrimeDetailFragment: Fragment() {
         val packageManager: PackageManager = requireActivity().packageManager
         val resolvedActivity: ResolveInfo? = packageManager.resolveActivity(intent, PackageManager.MATCH_DEFAULT_ONLY)
         return resolvedActivity != null
+    }
+
+    private fun updatePhoto(photoFileName: String?) {
+
+        // tag is used to prevent constant update of existing picture
+        if (binding.crimePhoto.tag != photoFileName) {
+            val photoFile = photoFileName?.let {
+                File(requireContext().applicationContext.filesDir, it)
+            }
+
+            if (photoFile?.exists() == true) {
+                binding.crimePhoto.doOnLayout { measuredView ->
+                    val scaledBitmap = getScaledBitmap(
+                        photoFile.path,
+                        measuredView.height,
+                        measuredView.width
+                    )
+                    binding.crimePhoto.setImageBitmap(scaledBitmap)
+                    binding.crimePhoto.tag = photoFileName
+                }
+            } else {
+                binding.crimePhoto.setImageBitmap(null)
+                binding.crimePhoto.tag = null
+            }
+        }
     }
 }
--- app/src/main/java/com/chondosha/criminalintent/CrimeRepository.kt
+++ app/src/main/java/com/chondosha/criminalintent/CrimeRepository.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import androidx.room.Room
 import com.chondosha.criminalintent.database.CrimeDatabase
 import com.chondosha.criminalintent.database.migration_1_2
+import com.chondosha.criminalintent.database.migration_2_3
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.flow.Flow
@@ -21,7 +22,7 @@ class CrimeRepository private constructor(
         context.applicationContext,
         CrimeDatabase::class.java,
         DATABASE_NAME
-    ).addMigrations(migration_1_2).build()
+    ).addMigrations(migration_1_2, migration_2_3).build()
 
     fun getCrimes(): Flow<List<Crime>> = database.crimeDao().getCrimes()
 
